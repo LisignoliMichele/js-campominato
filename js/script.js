f94c17e8edb0d2de7ec7011773a5f12ba78b74cf
Michele Lisignoli <michele@lisignoli.com>
2021-03-09 18:44:36 +0100

    campo minato

--- js/script.js
+++ js/script.js
@@ -21,19 +21,51 @@
 
 
 // FUNZIONI
-// funzione genera numeri tra 1 e 100 e i numeri non possono essere duplicati
-
 // function crea numeri random
-function randomNumber (min, max){
+function randomNumber ( min, max ){
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-//
+
+//function trova se é presente un elemento in un array
+function isItIn(array, element){
+  var i = 0;
+
+  while ( i < array.length ){
+    if ( array[i] == element ){
+      return true
+    }
+    i ++
+  }
+  return false;
+}
+
+
+
 // 1.creo 16 numeri casuali da 1 a 100, se non sono gia presenti gli inserisco in un array
 var cpuNumbers = [];
 while(cpuNumbers.length < 16){
     var casualNumber = randomNumber (1, 100);
-    if(cpuNumbers.indexOf(casualNumber) == -1){
+    if(isItIn(cpuNumbers, casualNumber) == false){
       cpuNumbers.push(casualNumber);
     }
 }
-console.log(cpuNumbers);
+alert(cpuNumbers)
+// 2. chiedo all’utente (100 - 16) volte di inserire un numero alla volta,
+var userNumbers = [];
+while (userNumbers.length < 64 && isItIn(cpuNumbers, entredNumber) == false){
+  var entredNumber = parseInt(prompt("please enter one number from 1 to 100"));
+  if ((entredNumber > 0 && entredNumber <= 100) && (isItIn(userNumbers, entredNumber) == false) ){
+        userNumbers.push(entredNumber);
+  }else if (isItIn(userNumbers, entredNumber)) {
+    alert("YOU already entred this number, try again")
+  }else{
+    alert("REMEMBER THE NUMBER MUST BE FROM 1 TO 100!!")
+  }
+}
+
+
+if (isItIn(cpuNumbers, entredNumber)){
+  alert("BOOOOOOOMM ahi ahi ahi you got a bomb!!!")
+}else{
+  alert("FANTASTIC you're the winner")
+}
